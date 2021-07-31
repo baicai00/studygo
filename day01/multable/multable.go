4527ea67525d5b9e0fbfa8d40d935ca0116074c6
baicai00 <dkai_2012@163.com>
2021-07-31 11:44:27 +0800

    提交

---
+++ day01/multable/multable.go
@@ -0,0 +1,13 @@
+/*99乘法表*/
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d*%d=%d ", i, j, i*j)
+		}
+		fmt.Println()
+	}
+}
